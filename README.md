750258e1381dc767af5b3808da57101a0d52425b
y <you.65535.kir@gmail.com>
2017-05-04 20:47:21 -0700

    Work in progress

---
+++ README.md
@@ -0,0 +1,1 @@
+FileEntityOnLinux by Yuichi Yoshii is licensed under the Apache License, Version2.0
--- src/FileEntityOnLinux.cpp
+++ src/FileEntityOnLinux.cpp
@@ -1,8 +1,22 @@
 /*
+ *
  * FileEntityOnLinux.cpp
  *
- *  Created on: 2017/02/23
- *      Author: mssqlserver
+ * Copyright 2016 Yuichi Yoshii
+ *     吉井雄一 @ 吉井産業  you.65535.kir@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #include "FileEntityOnLinux.h"
@@ -448,9 +462,6 @@ bool FileEntityOnLinux::IsCopySuccess() {
 }
 
 FileEntityOnLinux::FileEntityOnLinux() {
-    directory = string();
-    fileName = string();
-    fullPath = string();
     fileSize = 0;
     readAsBinary = false;
     readAsMB = false;
